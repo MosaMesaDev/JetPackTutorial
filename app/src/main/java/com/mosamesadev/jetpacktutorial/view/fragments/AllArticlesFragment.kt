4362f13bf5b8acf446f21e664388fd3ccdaa954c
Hendrik Brouwer <h.brouwer@gmail.com>
2021-12-01 11:23:52 +0100

    1.01 Added the 'add' + menu button in the homescreen.

--- app/src/main/java/com/mosamesadev/jetpacktutorial/view/fragments/AllArticlesFragment.kt
+++ app/src/main/java/com/mosamesadev/jetpacktutorial/view/fragments/AllArticlesFragment.kt
@@ -1,19 +1,25 @@
 package com.mosamesadev.jetpacktutorial.view.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.mosamesadev.jetpacktutorial.R
 import com.mosamesadev.jetpacktutorial.databinding.FragmentAllArticlesBinding
+import com.mosamesadev.jetpacktutorial.view.activities.AddUpdateActivity
 import com.mosamesadev.jetpacktutorial.viewmodel.AllArticlesViewModel
 
 class AllArticlesFragment : Fragment() {
 
     private lateinit var allArticlesViewModel: AllArticlesViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     private var _binding: FragmentAllArticlesBinding? = null
 
     // This property is only valid between onCreateView and
@@ -36,6 +42,22 @@ class AllArticlesFragment : Fragment() {
             textView.text = it
         })
         return root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_all_articles, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.action_add_article -> {
+                startActivity(Intent(requireActivity(), AddUpdateActivity::class.java))
+            return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onDestroyView() {
